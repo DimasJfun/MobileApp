d176fa4f293eea5f5875e28269c97f6df2f56ff2
DimasJfun <dimonrad5@gmail.com>
2020-02-10 23:05:59 +0300

    rer

---
+++ scanscript.js
@@ -0,0 +1,25 @@
+function barcodescanner(){
+    cordova.plugins.barcodeScanner.scan(
+    function (result) {
+    alert("We got a barcode\n" +
+    "Result: " + result.text + "\n" +
+    "Format: " + result.format + "\n" +
+    "Cancelled: " + result.cancelled + "\n" 
+    );
+    
+    },
+    function (error) {
+    alert("Scanning failed: " + error);
+    },
+    {
+        showFlipCameraButton : true,
+        showTorchButton : true,
+        torchOn: false,
+        saveHistory: true,
+        prompt : "Place a barcode inside the scan area",
+        resultDisplayDuration: 500,
+        formats : "QR_CODE,PDF_417",
+        orientation : "portrait",
+        disableSuccessBeep: false }
+        );
+        }
